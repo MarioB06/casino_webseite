517b8980aa7c12e928138376532e50b88dfa3d82
radblts <nevaroten@gmail.com>
2024-02-07 11:38:26 +0100

    worked on the ui of  the dashboard

--- casino_webseite/resources/js/Pages/Dashboard.vue
+++ casino_webseite/resources/js/Pages/Dashboard.vue
@@ -14,7 +14,21 @@ import { Head } from '@inertiajs/vue3';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
+                    <div class="p-6 text-gray-900 dark:text-gray-100">Welcome!</div>
+                </div>
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg" style="margin-top: 20px;">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">Money: </div>
+                </div>
+                <div class="flex" style="margin-left: 3%;">
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg" style="margin-top: 20px; margin-right: 3%; inline-size: 33.33%;" >
+                        <div class="p-6 text-gray-900 dark:text-gray-100">Slot Machine </div>
+                    </div>
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg" style="margin-top: 20px; margin-right: 3%; inline-size: 33.33%;">
+                        <div class="p-6 text-gray-900 dark:text-gray-100">Rocket Dice </div>
+                    </div>
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg" style="margin-top: 20px; margin-right: 3%; inline-size: 33.33%;">
+                        <div class="p-6 text-gray-900 dark:text-gray-100">Find the Cup </div>
+                    </div>
                 </div>
             </div>
         </div>
