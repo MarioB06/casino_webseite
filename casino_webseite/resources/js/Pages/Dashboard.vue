9f66e65f42a38bc523be0d1dd81a359357cc6157
radblts <nevaroten@gmail.com>
2024-02-21 09:45:35 +0100

    making the links from the game previews

--- casino_webseite/resources/js/Pages/Dashboard.vue
+++ casino_webseite/resources/js/Pages/Dashboard.vue
@@ -13,27 +13,36 @@ import { Head } from '@inertiajs/vue3';
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">Welcome!</div>
-                </div>
+                <h1 style="font-weight: bold; font-size: 2rem; padding: 1rem; text-align: center;">Herzlich willkommen im virtuellen Casino! <br> Tauchen Sie ein in die Welt des Glücks, ohne echtes Geld zu riskieren.</h1>
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg" style="margin-top: 1rem;">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">Money: </div>
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        <h2 style="font-weight: bold;">Money: </h2>
+                    </div>
                 </div>
                 <div class="flex">
-                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg" style="margin-top: 3rem; inline-size: 33.33% ; margin-right: 1rem;" >
-                        <div class="p-6 text-gray-900 dark:text-gray-100" style="text-align: center;">
-                            Slot Machine
-                        </div>
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500" style="margin-top: 3rem; inline-size: 33.33% ; margin-right: 1rem;;" >
+                        <a href="slotMachine">
+                            <div class="p-6 text-gray-900 dark:text-gray-100" style="text-align: center;">
+                                <h2 style="font-weight: bold;">Slot Machine</h2>
+                                <p style="text-align: left; margin-top: 1rem;">in this game you...</p>
+                            </div>
+                        </a>
                     </div>
-                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg" style="margin-top: 3rem; inline-size: 33.33% ; margin-right: 1rem;">
-                        <div class="p-6 text-gray-900 dark:text-gray-100" style="text-align: center;">
-                            Rocket Dice
-                        </div>
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500" style="margin-top: 3rem; inline-size: 33.33% ; margin-right: 1rem;">
+                        <a href="rocketDice">
+                            <div class="p-6 text-gray-900 dark:text-gray-100" style="text-align: center;">
+                                <h2 style="font-weight: bold;">Rocket Dice</h2>
+                                <p style="text-align: left; margin-top: 1rem;">in this game you...</p>
+                            </div>
+                        </a>
                     </div>
-                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg" style="margin-top: 3rem; inline-size: 33.33%">
-                        <div class="p-6 text-gray-900 dark:text-gray-100" style="text-align: center;">
-                            Find the Cup
-                        </div>
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500" style="margin-top: 3rem; inline-size: 33.33%">
+                        <a href="findTheCup">
+                            <div class="p-6 text-gray-900 dark:text-gray-100" style="text-align: center;">
+                                <h2 style="font-weight: bold;">Find the Cup</h2>
+                                <p style="text-align: left; margin-top: 1rem;">in this game you...</p>
+                            </div>
+                        </a>
                     </div>
                 </div>
             </div>
