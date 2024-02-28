12f2e6f0e7a0b210235f57ae114c6eb77aed7ca7
MarioB06 <mbozic2006@gmail.com>
2024-02-28 10:39:54 +0100

    route for game start for rocket dice

--- casino_webseite/routes/web.php
+++ casino_webseite/routes/web.php
@@ -55,6 +55,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('/rocketdice/play', [RocketDiceController::class, 'play'])->name('rocketdice.play');
+
+    //{{ route('rocketdice.play') }}
 });
 
 require __DIR__.'/auth.php';
