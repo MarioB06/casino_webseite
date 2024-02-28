0f48a610e79d2d6221d911c82f3ad49442c839f5
INABIK4RI <rubenedgar34@gmail.com>
2024-02-28 10:30:31 +0100

    Dice img

--- casino_webseite/resources/js/Pages/RocketDice.vue
+++ casino_webseite/resources/js/Pages/RocketDice.vue
@@ -15,7 +15,7 @@ import { Head } from '@inertiajs/vue3';
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900 dark:text-gray-100">
-                        hello
+                        hello!
                     </div>
                 </div>
             </div>
