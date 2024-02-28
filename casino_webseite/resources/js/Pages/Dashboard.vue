f3ca05ccc9026bc8170947480464aae9c473e912
radblts <nevaroten@gmail.com>
2024-02-28 10:31:15 +0100

    formular erstellt

--- casino_webseite/resources/js/Pages/Dashboard.vue
+++ casino_webseite/resources/js/Pages/Dashboard.vue
@@ -15,26 +15,30 @@ import { Head } from '@inertiajs/vue3';
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">Welcome!</div>
+                <div style="font-size: 2rem; font-weight: bold; text-align: center;">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">Herzlich willkommen im virtuellen Casino! <br> Tauchen Sie ein in die Welt des Glücks, ohne echtes Geld zu riskieren.</div>
                 </div>
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg" style="margin-top: 1rem;">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">Money: {{ $attrs.account }} CHF </div>
+                    <div class="p-6 text-gray-900 dark:text-gray-100">Kontostand: {{ $attrs.account }} CHF </div>
                 </div>
                 <div class="flex">
-                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg" style="margin-top: 3rem; inline-size: 33.33% ; margin-right: 1rem;" >
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg motion-safe:hover:scale-[1.05] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500" style="margin-top: 3rem; inline-size: 33.33% ; margin-right: 1rem;" >
                         <div class="p-6 text-gray-900 dark:text-gray-100" style="text-align: center;">
-                            Slot Machine
+                            <p style="font-weight: bold; font-size: large;">Slot Machine</p>
+                            <p style="text-align: left;">Setze Einsatz, drehe Walzen. Gewinne bei passenden Symbolen.</p>
                         </div>
                     </div>
-                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg" style="margin-top: 3rem; inline-size: 33.33% ; margin-right: 1rem;">
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg motion-safe:hover:scale-[1.05] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500" style="margin-top: 3rem; inline-size: 33.33% ; margin-right: 1rem;">
                         <div class="p-6 text-gray-900 dark:text-gray-100" style="text-align: center;">
-                            Rocket Dice
+                            <p style="font-weight: bold; font-size: large;">Rocket Dice</p>
+                            <p style="text-align: left;">Im Spiel werden zwei Würfel verwendet. Der Spieler platziert einen Einsatz, wählt "Über" oder "Unter" sowie die gewünschte Zahl und wirft die Würfel. Bei korrekter Vorhersage gewinnt der Spieler.</p>
                         </div>
                     </div>
-                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg" style="margin-top: 3rem; inline-size: 33.33%">
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg motion-safe:hover:scale-[1.05] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500" style="margin-top: 3rem; inline-size: 33.33%">
                         <div class="p-6 text-gray-900 dark:text-gray-100" style="text-align: center;">
-                            Find the Cup
+                            <p style="font-weight: bold; font-size: large;">Find the Cup</p>
+                            <p style="text-align: left;">Wähle den Wert, mit dem du spielen möchtest. Drücke auf den 'Drehen'-Button oder wiederhole den Einsatz, um zu spielen.
+                                Gewinne, wenn die Symbole auf einer aktivierten Gewinnlinie übereinstimmen!</p>
                         </div>
                     </div>
                 </div>
